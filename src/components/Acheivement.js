db2735213e096e83fe5979db0cc6a8b29e343663
Siraj <ksiraj728@gmail.com>
2024-07-12 16:38:48 +0530

    imagesizeincrease

--- src/components/Acheivement.js
+++ src/components/Acheivement.js
@@ -3,15 +3,15 @@ const Acheivement = () => {
     <section>
       <div className="  sm:flex justify-center w-full  font-saira">
         <div className="left sm:relative sm:z-10 sm:left-36 sm:w-2/5 sm:mt-44  xs:px-4 xs:pt-4 ">
-        <h1 className=" xs:text-3xl sm:text-4xl font-semibold  text-textBlue leading-10  text-center tracking-wider " >
+        <h1 className=" xs:text-2xl sm:text-4xl font-semibold  text-textBlue leading-10  text-center tracking-wider " >
           <span className="text-textRed"> SPORTS </span> IS NOT ABOUT WHAT YOU <span className="text-textRed " >ACHEIVE</span> <br></br>IT'S ALL ABOUT <span className="text-textRed" >WHO</span> AND<span className="text-textRed" > WHAT</span> YOU
           BECOME
         </h1>
-        <h1 className="font-medium leading-snug xs:text-3xl xs:mt-3 sm:text-4xl italic sm:mt-8 text-center">Our site is getting a little <br/>tune up and some love.</h1>
+        <h1 className="font-medium leading-snug xs:text-2xl xs:mt-3 sm:text-4xl italic sm:mt-8 text-center">Our site is getting a little <br/>tune up and some love.</h1>
         </div>
         
          <div className="right sm:w-3/4  xs:mt-4 sm:ml-9 ">
-         <img src="/image/rahulrane.png" alt="sir " className=" sm:h-[692px]  xs:p-4  xs:mb-5  xs:mt-4   "/>
+         <img src="/image/rahulrane.png" alt="sir " className=" sm:h-[692px]    xs:mb-5  xs:mt-4   "/>
   </div>
 
 
